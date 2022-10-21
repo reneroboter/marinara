45019624cd21a3abf67d4791fde539ba53f37a9f
René Backhaus <rene.backhaus@axelspringer.com>
2022-10-21 11:39:33 +0200

    Debug

--- src/background/Blocker.js
+++ src/background/Blocker.js
@@ -8,7 +8,11 @@ class Blocker
 
   async apply() {
     let { blockListDomains: urlsToBeBlocked } = await SettingsClient.once.getSettings();
-
+    console.log(urlsToBeBlocked)
+    console.log(chrome.webRequest.onBeforeRequest.hasListeners())
+    // wenn schon ein listener vorhanden ist, muss der alte ersetzt werden, weil sonst alle urls im speicher bleiben?!
+    // todo check if all urls where valid or filter ..
+    // https://stackoverflow.com/questions/23001428/chrome-webrequest-onbeforerequest-removelistener-how-to-stop-a-chrome-web
     chrome.webRequest.onBeforeRequest.addListener(() => {
           if (this.timer.phase === Phase.Focus && this.timer.isRunning) {
             return {cancel: true};
